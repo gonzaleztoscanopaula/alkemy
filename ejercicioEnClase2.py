3f3907727da22dc75e7a19e2793e2f6b1e8b3928
Paula González toscano <gonzaleztoscano.pau@gmail.com>
2024-03-11 20:12:12 -0300

    "ej clases metodos y atributos finalizado"

--- ejercicioEnClase2.py
+++ ejercicioEnClase2.py
@@ -37,7 +37,7 @@ class Pelicula(Producto):
         self.sagas = sagas
 
     def mostrarProducto(self):
-        return print(f"La categoria de la película es {self.categoria}, su precio es {self.precio}, la cantidad en stock disponible es {self.cantidadEnStock}, el nombre dela película es {self.nombre}, el genero de la pelicula es {self.genero}, y esta pelicula tiene {self.sagas} sagas.")
+        return print(f"La categoria de la película es {self.categoria}, su precio es {self.precio}, la cantidad en stock disponible es {self.cantidadEnStock}, el nombre dela película es {self.nombre}, el genero de la pelicula es {self.genero}, y existen {self.sagas} pelicula de la saga.")
     
 class Disco(Producto):
 
@@ -53,12 +53,25 @@ class Disco(Producto):
 
 
 libro1 = Libro("Ficción", 20.99, 50, "El Señor de los Anillos", "J.R.R. Tolkien")
+libro2 = Libro("Fantasía", 15.99, 30, "Harry Potter y la Piedra Filosofal", "J.K. Rowling")
+libro3 = Libro("Suspenso", 18.50, 25, "El Código Da Vinci", "Dan Brown")
 
 
 pelicula1 = Pelicula("Acción", 15.50, 100, "Matrix", "Ciencia Ficción", "3")
+pelicula2 = Pelicula("Comedia", 12.99, 80, "Mi Pobre Angelito", "Comedia", 6)
+pelicula3 = Pelicula("Drama", 17.50, 40, "Forrest Gump", "Drama", 0)
 
 
 disco1 = Disco("Rock", 12.99, 30, "Queen", "Rock", 1980)
+disco2 = Disco("Pop", 14.99, 50, "Michael Jackson", "Pop", 1982)
+disco3 = Disco("Electrónica", 19.99, 20, "Daft Punk", "Electrónica", 2001)
+
+
+
+
+
+
+
 
 
 print (libro1.mostrarProducto())
